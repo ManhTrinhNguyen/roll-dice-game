44b118d0ba8ac1096b33b1c059321e350d9cd946
ManhTrinhNguyen <nguyenmanhtrinh17041998@gmail.com>
2023-02-17 13:02:17 -0500

    Update Attack Btn and Score

--- data.js
+++ data.js
@@ -3,14 +3,14 @@ const characterData = {
         elementId: "hero",
         name: "Wizard",
         avatar: "images/wizard.png",
-        health: 60,
+        health: 100,
         diceCount: 3
     },
     monster: {
         elementId: "monster",
         name: "Orc",
         avatar: "images/orc.png",
-        health: 10,
+        health: 100,
         diceCount: 1
     }
 }
